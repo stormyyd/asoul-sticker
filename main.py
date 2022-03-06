055800d96306138860b33ddf39fe11876c9e39a3
Yingdong Yang <storm-yyd@outlook.com>
2022-03-06 12:37:47 +0800

    优化实现

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import json
 import string
 
 def main():
-    css_items = []
     with open('config.json', encoding='utf8') as f:
         config = json.load(f)
-    with open('style_template.css', encoding='utf8') as f:
-        css_template = string.Template(f.read())
     flatten = config["flatten"]
+
+    with open('asoul-sticker-base.css', encoding='utf8') as f:
+        css_base = string.Template(f.read())
+    with open('asoul-sticker-template.css', encoding='utf8') as f:
+        css_template = string.Template(f.read())
+
+    css_items = []
     with open('data.txt', encoding='utf8') as f:
         for line in [l.strip() for l in f]:
             if len(line) == 0 or line[0] == '#':
@@ -16,12 +20,14 @@ def main():
             if flatten:
                 host = config['host']
             else:
-                host = config['host'] + '/' + dir_name
-            css_item = css_template.substitute(className=class_name, host=host, filename=file_name, height=config['height'])
+                host = f'{config["host"]}/{dir_name}'
+            css_item = css_template.substitute(className=class_name, host=host, filename=file_name, alt=f'[{alt_name}]')
             css_items.append(css_item)
-    css_content = ''.join(css_items)
+
     with open('dist/asoul-sticker.css', 'w', encoding='utf8', newline='\n') as f:
-        f.write(css_content)
+        f.write(css_base.substitute(height=config['height']))
+        f.write('\n')
+        f.write(''.join(css_items))
 
 if __name__ == '__main__':
     main()

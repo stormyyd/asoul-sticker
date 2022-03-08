fbb346c767e4fd3ff5af7ddc83f54912f73501d9
Yingdong Yang <storm-yyd@outlook.com>
2022-03-08 22:22:38 +0800

    将设置表情包的统一样式的selector也交由生成器自动生成

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import string
 
-_VERSION = '0.3.1'
+_VERSION = '0.3.2'
 
 def main():
     with open('config.json', encoding='utf8') as f:
@@ -13,23 +13,37 @@ def main():
     with open('asoul-sticker-template.css', encoding='utf8') as f:
         css_template = string.Template(f.read())
 
-    css_items = []
+    sticker_items = []
+    class_prefixes = set()
     with open('data.txt', encoding='utf8') as f:
         for line in [l.strip() for l in f]:
             if len(line) == 0 or line[0] == '#':
                 continue
             dir_name, file_name, class_name, alt_name = line.split(' ')
+            class_prefixes.add(dir_name)
             if config['flatten']:
                 host = config['host']
             else:
                 host = f'{config["host"]}/{dir_name}'
-            css_item = css_template.substitute(className=class_name, host=host, filename=file_name, alt=f'[{alt_name}]')
-            css_items.append(css_item)
+            css_item = css_template.substitute(
+                className=class_name,
+                host=host,
+                filename=file_name,
+                alt=f'[{alt_name}]',
+            )
+            sticker_items.append(css_item)
 
     with open('dist/asoul-sticker.css', 'w', encoding='utf8', newline='\n') as f:
-        f.write(css_base.substitute(version=_VERSION, host=config["host"], height=config['height'], flatten=str(config['flatten']).lower()))
+        selectors = [f'span[class^="{prefix}_"]' for prefix in sorted(class_prefixes)]
+        f.write(css_base.substitute(
+            version=_VERSION,
+            host=config["host"],
+            height=config['height'],
+            flatten=str(config['flatten']).lower(),
+            selectors=',\n'.join(selectors)
+        ))
         f.write('\n')
-        f.write(''.join(css_items))
+        f.write(''.join(sticker_items))
 
 if __name__ == '__main__':
     main()
